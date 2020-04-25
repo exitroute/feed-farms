d41073900831e4c63d83b9cb90f6661efaf1a66a
Ryan O'Shea <ryan.oshea@exitroute.org>
2020-04-25 17:43:55 +0200

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import { Center, Stack, Box } from "../components/EveryLayout";
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
+`;
 
-export default () => <Title>My page</Title>
+const Home = () => (
+  <Center>
+    <Stack>
+      <Box>
+        <Title>FeedFarms</Title>
+      </Box>
+    </Stack>
+  </Center>
+);
+
+export default Home;
