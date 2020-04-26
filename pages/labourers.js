f836323b5fe0c1e57fce0a7be1502c9453f0ed07
Ryan O'Shea <ryan.oshea@exitroute.org>
2020-04-26 10:16:21 +0200

    add profile

--- pages/labourers.js
+++ pages/labourers.js
@@ -1,17 +1,37 @@
 import React from "react";
 import { Cover, Center, Stack, Box } from "../components/EveryLayout";
-
 import Layout from "../components/Layout";
+
+import dynamic from "next/dynamic";
+
+import styled from "styled-components";
+
+const DynamicComponentWithNoSSR = dynamic(() => import("../components/Map"), {
+  ssr: false
+});
+
+const List = styled.ul`
+  list-style-type: none;
+`;
 
 const Labourers = () => (
   <Layout>
     <Cover centered={"div"} minHeight={"50vh"}>
       <div>
         <Center>
-          <h1>FeedFarms</h1>
-          <p>A project for EUvsCorona</p>
+          <h1>Profile</h1>
+          <List>
+            <li>Name</li>
+            <li>Location</li>
+            <li>Availability</li>
+            <li>Skill</li>
+            <li>Experience</li>
+            <li>Right to Work</li>
+            <li>Team or Individual</li>
+          </List>
         </Center>
       </div>
+      <DynamicComponentWithNoSSR />
     </Cover>
   </Layout>
 );
