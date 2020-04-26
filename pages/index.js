d9596e857901000bb5e024f8acc5f7062d01b6c0
Ryan O'Shea <ryan.oshea@exitroute.org>
2020-04-26 10:17:09 +0200

    and pictures

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,33 @@
 import React from "react";
-import { Cover, Center, Stack, Box, Switcher } from "../components/EveryLayout";
-import SideBar from "../components/SideBar";
+import styled from "styled-components";
+import {
+  Cover,
+  Center,
+  Stack,
+  Box,
+  Frame,
+  Switcher
+} from "../components/EveryLayout";
 
 import Layout from "../components/Layout";
+
+const UserComments = styled(Box)`
+  text-align: center;
+`;
 
 const Home = () => (
   <Layout>
     <Box>
       <h1>Information about the platform</h1>
-      <img src="https://picsum.photos/600/400"></img>
+      <Frame>
+        <img src="https://picsum.photos/600/400"></img>
+      </Frame>
     </Box>
     <Switcher>
       <div>
-        <Box>User content</Box>
-        <Box>User content</Box>
-        <Box>User content</Box>
+        <UserComments borderThin="1px">This site is amazing</UserComments>
+        <UserComments borderThin="1px">Wow!</UserComments>
+        <UserComments borderThin="1px">This is changing my life</UserComments>
       </div>
     </Switcher>
   </Layout>
