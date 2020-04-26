10799bb664d0ca4652448a04cdc7d280731e40fb
Ryan O'Shea <ryan.oshea@exitroute.org>
2020-04-26 08:56:26 +0200

    add index

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,23 @@
 import React from "react";
-import styled from "styled-components";
-import { Center, Stack, Box } from "../components/EveryLayout";
+import { Cover, Center, Stack, Box, Switcher } from "../components/EveryLayout";
+import SideBar from "../components/SideBar";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`;
+import Layout from "../components/Layout";
 
 const Home = () => (
-  <Center>
-    <Stack>
-      <Box>
-        <Title>FeedFarms</Title>
-      </Box>
-    </Stack>
-  </Center>
+  <Layout>
+    <Box>
+      <h1>Information about the platform</h1>
+      <img src="https://picsum.photos/600/400"></img>
+    </Box>
+    <Switcher>
+      <div>
+        <Box>User content</Box>
+        <Box>User content</Box>
+        <Box>User content</Box>
+      </div>
+    </Switcher>
+  </Layout>
 );
 
 export default Home;
