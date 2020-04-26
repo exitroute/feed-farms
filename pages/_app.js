20685e4ceb809b96c82c455171e29879bd8707b9
Ryan O'Shea <ryan.oshea@exitroute.org>
2020-04-26 08:55:54 +0200

    add styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,39 @@
 import App from "next/app";
 import React from "react";
-import { ThemeProvider } from "styled-components";
+import { createGlobalStyle } from "styled-components";
 
-const theme = {
-  colors: {
-    primary: "black"
+const GlobalStyle = createGlobalStyle`
+   :root {
+  --ratio: 1.5;
+  --s-5: calc(var(--s-4) / var(--ratio));
+  --s-4: calc(var(--s-3) / var(--ratio));
+  --s-3: calc(var(--s-2) / var(--ratio));
+  --s-2: calc(var(--s-1) / var(--ratio));
+  --s-1: calc(var(--s0) / var(--ratio));
+  --s0: 1rem;
+  --s1: calc(var(--s0) * var(--ratio));
+  --s2: calc(var(--s1) * var(--ratio));
+  --s3: calc(var(--s2) * var(--ratio));
+  --s4: calc(var(--s3) * var(--ratio));
+  --s5: calc(var(--s4) * var(--ratio));
+  --measure: 80ch;
+  --color-light: #fff;
+  --color-dark: #000;
+}  
+   body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir, Helvetica, sans-serif;
   }
-};
+`;
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyle />
         <Component {...pageProps} />
-      </ThemeProvider>
+      </>
     );
   }
 }
